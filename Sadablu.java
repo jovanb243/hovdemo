137faf3ae4b892e85da73bddf867663444a10f9f
jovanb243 <jovanb243@gmail.com>
2020-06-21 02:59:27 -0400

    First java program

---
+++ Sadablu.java
@@ -0,0 +1,9 @@
+public class Sadablu {
+	
+	public static void main(String[] args) {
+		// Prints "Welcome to Sadablu" to the terminal window.
+		System.out.println("Welcome to sadablu");
+	}
+	
+}	  
+
